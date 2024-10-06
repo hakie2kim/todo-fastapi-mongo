649a7bd3f3215edf59c4f2ea3916a7a9a189afec
Hakjun Kim <hakie2kim@gmail.com>
2024-10-06 16:05:25 +0900

    etc: Mongodb와 연결 설정

---
+++ config/database.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+import os
+from dotenv import load_dotenv
+
+import urllib.parse
+
+# .env 파일에서 환경 변수 로드
+load_dotenv()
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+
+client = MongoClient(f"mongodb+srv://{urllib.parse.quote_plus(db_user)}:{urllib.parse.quote_plus(db_password)}@cluster0.w1u8l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+db = client.todo_db
+collection_name = db["todo_collection"]
